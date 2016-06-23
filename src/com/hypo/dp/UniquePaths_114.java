193ba1ccfa0b534b97b0576a61ee0c638e4d9e0d
863153658@qq.com <863153658@qq.com>
2016-06-23 19:18:27 +0800

    不同的路径 [easy]

---
+++ src/com/hypo/dp/UniquePaths_114.java
@@ -0,0 +1,26 @@
+package com.hypo.dp;
+
+public class UniquePaths_114
+{
+    public int uniquePaths(int m, int n) 
+    {
+    	int[][] f = new int[m][n];
+    	
+    	//只能向下或向右移动
+    	for(int i = 0 ; i < m ; ++i)
+    	{
+    		for(int j = 0 ; j < n ; ++j)
+    		{
+    			if(i == 0 || j == 0)
+    			{
+    				f[i][j] = 1;
+    			}
+    			else
+    			{
+    				f[i][j] = f[i-1][j] + f[i][j-1];
+    			}
+    		}
+    	}
+    	return f[m-1][n-1];
+    }
+}
