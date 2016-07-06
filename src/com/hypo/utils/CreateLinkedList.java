3d760f742875badf094ad997dc460002b510f1c9
863153658@qq.com <863153658@qq.com>
2016-07-06 09:53:56 +0800

    由给定的一维数组建链表

---
+++ src/com/hypo/utils/CreateLinkedList.java
@@ -0,0 +1,53 @@
+package com.hypo.utils;
+
+//由给定的一维数组建链表
+public class CreateLinkedList
+{
+	public ListNode create(int[] nums)
+	{
+		if(nums.length < 1) return null;
+		
+		ListNode dummy = new ListNode(-1);
+		ListNode prev = dummy;
+		
+		for(int i = 0 ; i < nums.length ; ++i)
+		{
+			ListNode newnode = new ListNode(nums[i]);
+			prev.next = newnode;
+			
+			prev = newnode;
+		}
+		return dummy.next;
+	}
+	
+	private void print(ListNode node)
+	{
+		while(node != null)
+		{
+			System.out.print(node.val + " ");
+			node = node.next;
+		}
+		System.out.println();
+	}
+	
+	//test
+	public static void main(String[] args)
+	{
+		CreateLinkedList cll = new CreateLinkedList();
+		
+		int[] nums1 = {};
+		int[] nums2 = {1};
+		int[] nums3 = {1,2};
+		int[] nums4 = {0,1,3,4,4,5,6};
+		
+		ListNode n1 = cll.create(nums1);
+		ListNode n2 = cll.create(nums2);
+		ListNode n3 = cll.create(nums3);
+		ListNode n4 = cll.create(nums4);
+		
+		cll.print(n1);
+		cll.print(n2);
+		cll.print(n3);
+		cll.print(n4);
+	}
+}
