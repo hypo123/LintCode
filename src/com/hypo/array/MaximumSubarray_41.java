2052d96efce05e222f87074a2846778bb0cb42dc
863153658@qq.com <863153658@qq.com>
2016-08-10 17:14:31 +0800

    最大子数组和	[easy]

--- src/com/hypo/array/MaximumSubarray_41.java
+++ src/com/hypo/array/MaximumSubarray_41.java
@@ -8,12 +8,13 @@ public class MaximumSubarray_41
 {
     public int maxSubArray(int[] nums) 
     {
-    	int sum = Integer.MIN_VALUE;
+    	int sum = Integer.MIN_VALUE;//全局最大子数组和
     	
-    	int current_sum = 0;
+    	int current_sum = 0;//当前子数组和
     	
     	for(int n : nums)
     	{
+    		//如果当前子数组和小于0,则对后面的子数组没有帮助,舍弃
     		if(current_sum < 0)
     		{
     			sum = Math.max(sum, current_sum);
