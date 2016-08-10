7cb6017abab09cb4d3e506e1874481567364273c
863153658@qq.com <863153658@qq.com>
2016-08-10 17:12:13 +0800

    最大子数组测试

---
+++ src/com/hypo/array/test/MaximumSubarray_41Test.java
@@ -0,0 +1,41 @@
+package com.hypo.array.test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.hypo.array.MaximumSubarray_41;
+
+public class MaximumSubarray_41Test
+{
+	MaximumSubarray_41 t41;
+	@Before
+	public void setUp() throws Exception
+	{
+		 t41 = new MaximumSubarray_41();
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void testMaxSubArray1()
+	{
+		int[] nums = { -2,2,-3,4,-1,2,1,-5,3 };
+		int expt = 6;
+		org.junit.Assert.assertEquals(expt, t41.maxSubArray(nums));
+	}
+	
+	@Test
+	public void testMaxSubArray2()
+	{
+		int[] nums = { -6,-5,-1,-5,-10 };
+		int expt = -1;
+		org.junit.Assert.assertEquals(expt, t41.maxSubArray(nums));
+	}
+
+}
