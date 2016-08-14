2865beb402208f0df76da189450370c58489ca7b
863153658@qq.com <863153658@qq.com>
2016-08-14 21:44:16 +0800

    红黑树

--- src/com/hypo/utils/RedBlackTree.java
+++ src/com/hypo/utils/RedBlackTree.java
@@ -8,5 +8,52 @@ package com.hypo.utils;
  */
 public class RedBlackTree
 {
-//TODO
+	private static final boolean RED = true;
+	private static final boolean BLACK = false;
+	
+	//红黑树结点类
+	private class Node
+	{
+		private int key;
+		private int value;
+		private Node left;
+		private Node right;
+		private boolean color;
+		private int size;//子树数量
+		
+		public Node(int key , int value ,boolean color , int size)
+		{
+			this.key = key;
+			this.value = value;
+			this.color = color;
+			this.size = size;
+		}
+	}
+	
+   /***************************************************************************
+    *  结点类Node辅助方法.
+    ***************************************************************************/
+		
+	
+	
+	
+	
+	
+	
+	
+   /***************************************************************************
+    *  标准BST树查找.
+    ***************************************************************************/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
