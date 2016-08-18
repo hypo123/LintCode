569dba5df9600876793cc0c769d91950507ceea9
863153658@qq.com <863153658@qq.com>
2016-08-18 11:10:15 +0800

    坐标点类

--- src/com/hypo/utils/Point.java
+++ src/com/hypo/utils/Point.java
@@ -3,8 +3,8 @@ package com.hypo.utils;
 //坐标点类
 public class Point
 {
-    int x;//横坐标
-    int y;//纵坐标
-    Point() { x = 0; y = 0; }
-    Point(int a, int b) { x = a; y = b; }
+    public int x;//横坐标
+    public int y;//纵坐标
+    public Point() { x = 0; y = 0; }
+    public Point(int a, int b) { x = a; y = b; }
 }
