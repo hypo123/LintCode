c8d25959821d0f29a774cf746184c1c16150e6df
863153658@qq.com <863153658@qq.com>
2016-08-18 20:57:13 +0800

    Boyer-Moore字符串查找算法

---
+++ src/com/hypo/utils/test/BoyerMoore_Test.java
@@ -0,0 +1,34 @@
+package com.hypo.utils.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoyerMoore_Test
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void testSearchString()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchCharArray()
+	{
+		fail("Not yet implemented");
+	}
+
+}
