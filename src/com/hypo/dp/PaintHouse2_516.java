6ca4246e35be9cdca224612cccfdc368c9414944
863153658@qq.com <863153658@qq.com>
2016-08-31 20:46:52 +0800

    房屋染色 II	[hard]

---
+++ src/com/hypo/dp/PaintHouse2_516.java
@@ -0,0 +1,27 @@
+package com.hypo.dp;
+
+/**
+ * 	房屋染色 II	[hard]
+ *	这里有n个房子在一列直线上，现在我们需要给房屋染色，分别有红色蓝色和绿色。每个房屋染不同的颜色费用也不同，你需要设计一种染色方案使得相邻的房屋颜色不同，并且费用最小。
+	费用通过一个nx3 的矩阵给出，比如cost[0][0]表示房屋0染红色的费用，cost[1][2]表示房屋1染绿色的费用。
+
+ 	注意事项
+	所有费用都是正整数
+	
+	样例:
+	costs = [[14,2,11],[11,14,5],[14,3,10]] return 10
+	房屋 0 蓝色, 房屋 1 绿色, 房屋 2 蓝色， 2 + 5 + 3 = 10
+	
+	用O(nk)的时间复杂度解决 见PaintHouse_515.java
+ */
+public class PaintHouse2_516
+{
+    /**
+     * @param costs n x k cost matrix
+     * @return an integer, the minimum cost to paint all houses
+     */
+    public int minCostII(int[][] costs) 
+    {
+    	return 0;
+    }
+}
